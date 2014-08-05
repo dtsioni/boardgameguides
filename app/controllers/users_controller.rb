14153a9ea52d30b6efc7b337f52e8c617059fe31
Daniel Tsioni <dtsioni@gmail.com>
2014-08-05 11:04:12 -0400

    Finished modeling user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,34 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:show]
+
   def show
   end
+
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
+  end  
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html{redirect_to @user}
+      else
+        format.html{render :new}
+      end
+    end
+  end
+
   private
     def set_user
-      @user = User.find(params[:id]
+      @user = User.find(params[:id])
     end
+    def user_params
+      params.require(:user).permit(:name, :email, :role, :rank, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :index]
 end
